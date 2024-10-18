6434072eae0ec33b95ed37fcee6e0558248185de
JohnathonBao <163490447+JohnathonBao@users.noreply.github.com>
2024-10-17 17:19:09 -0700

    Create hw2.py

---
+++ hw2.py
@@ -0,0 +1,56 @@
+'''
+Definitions
+Arithmetic operator: math symbol that modifies numbers
+comparison operators: math symbol that compares numbers
+logical operators: operator that checks if conditions are true (and, or, not)
+Assignment operators: operator that assigns things
+
+'''
+
+x=10
+y=4
+
+print('sum: 14')
+print('difference: 6')
+print('product: 40')
+print('floor division: 0.4')
+
+a=8
+b=15
+
+print(True)
+print(False)
+print(True)
+
+is_raining=True
+is_windy=False
+
+if is_raining and is_windy:
+    print('stay inside')
+if is_raining or is_windy:
+    print('go for a walk')
+if not is_raining:
+    print("it's not raining")
+print()
+
+c=5
+print(c)
+c+=10
+print(c)
+c-=3
+print(c)
+c*=2
+print(c)
+c/=4
+print(c)
+print()
+
+'''don't know how to do this
+def fizz_buzz():
+    n=int(input('Enter a number: '))
+    if n/3 and n/5:
+        print('FizzBuzz')
+    if n/3!=float and not n/5!=float:
+        print('Fizz')
+fizz_buzz()
+'''
