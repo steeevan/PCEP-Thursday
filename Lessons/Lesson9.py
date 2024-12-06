28aab923685b474b9b989a03daf006628f457975
steeevan <estevananguiano99@yahoo.com>
2024-12-05 18:50:02 -0800

    Exercises

---
+++ Lessons/Lesson9.py
@@ -0,0 +1,30 @@
+'''
+Lesson 9
+Functions
+    - Understand what a function is and its role in programming
+    - learn how to defin and invoke functinos in python
+    - debug commmon function-related issues
+    - Explore different use cases for functions
+
+'''
+
+'''
+Function - A block of organized, resusable code that performs a single action. Functinos help break donw complex 
+            problems into smaller, manageble pieces
+
+'''
+
+def function_name(parameters):
+    """docstring (optional)"""
+    # code block 
+    return value
+
+# We use the keywork 'def' to initizialize the function
+# Invocation is the process of calling or executing a function to perform its task
+
+# Create a greeting functino
+# write a functino that takes a users name and age as arguments and prints a personalize message
+
+# Lets look into exercise folder 
+# Exercise Lesson 9E
+
