8b98524b63310bd32850ca1e0010a48b1ff72103
steeevan <estevananguiano99@yahoo.com>
2025-01-30 20:03:53 -0800

    Expressions

---
+++ Lessons/Lesson14.py
@@ -0,0 +1,53 @@
+'''
+Operator and Precedence
+You will determine the output of a Python calculation
+or expression. Each questions is presented as a multiple choice. 
+'''
+
+#1 Example
+# what will be printed out by the code snippet below?
+print(5 * 2 + 8 / 4)
+
+# 2 Example
+print(7/2*2)
+
+
+# Example 3
+print( 2 ** -1 + 5 // 2)
+
+# Example 4
+print((4 - 2) ** 3 / 2 + 1)
+
+#-
+# Example 5
+print(3 * 2 ** 3)
+# 6
+print(8 / (2 + 2) ** 2)
+#7
+print(-10 % 3)
+#8
+print(-3 ** 2)
+#9
+x = 2
+x *= 3 + 2
+x /= 2
+print(x)
+
+#10
+print(1 + 2 ** 3 // 4 * 3.0)
+
+print(3 + 4 * 2)
+print((2 + 5) // 3 * 2)
+print(10 / 5 + 3 * 2)
+print(10 // 5 + 3 / 2)
+print(4 ** 2 - 2 ** 3)
+print(3 + 3 * (2 + 4))
+
+
+print((2 + 3) ** 2 / (4 - 2))
+print(10 - 8 // 3)
+print(10 - 8 / 3)
+print(4 * 3 // 2 + 7 % 4)
+print(2 * (5 + 3) / 4)
+print((3 + 4) / 2 * 3)
+print(25 // 10.0)
